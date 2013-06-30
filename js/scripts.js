66c08759cf207d93d284e7df8d6d04182a0336f6
Bryan Zavestoski <bzavestoski@gmail.com>
2013-06-30 12:14:07 -0700

    Update loading JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ $(window).load(function(){
   $("#mask").fadeOut(1000);
   $(".container").addClass("movement");
 });
+
+
+// $(".thanks").hide();
+// $("#mc-embedded-subscribe").on("click", function(){
+//   $(".thanks").show();
+// });
